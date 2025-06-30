c4befccf5a187f19ac8828917bab91266aeeb12b
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-06-30 12:40:01 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,55 +1,6 @@
 Config = {}
 
-Config.Traps = {
-    {
-        Item = 'beartrap',
-        Model = `s_beartrapanimated01x`, 
-        FallbackModel = `s_beartrapanimated01x`, 
-        Baits = {
-            { 
-                item = nil, 
-                animals = { 'a_c_bear_01', 'a_c_wolf_01', 'a_c_cougar_01' }, 
-                chance = 75 
-            },
-            { 
-                item = nil, 
-                animals = { 'a_c_fox_01', 'a_c_coyote_01', 'a_c_raccoon_01', 'a_c_boar_01' }, 
-                chance = 60 
-            },
-            { 
-                item = nil, 
-                animals = { 'a_c_deer_01', 'a_c_elk_01', 'a_c_pronghorn_01' }, 
-                chance = 50 
-            },
-            {
-                item = nil,
-                animals = { 
-                    'a_c_rabbit_01', 'a_c_squirrel_01', 'a_c_chipmunk_01',
-                    'a_c_fox_01', 'a_c_coyote_01', 'a_c_wolf_01' 
-                },
-                chance = 25
-            }
-        },
-        CaptureDuration = 20,
-        Damage = 90,
-        Animation = 'WORLD_HUMAN_CROUCH_INSPECT',
-        MaxCaptures = 3,
-        PlacementTime = 5000
-    }
-}
 
-
-
-
-Config.InteractionDistance = 2.5
-Config.CheckRadius = 2.0 -- Radius for animal detection
-Config.SpawnRadius = 60.0 -- Max radius for spawning random animal
-Config.MinSpawnDistance = 5.0 -- Minimum distance from player for animal spawn
-Config.AnimalsPerTrap = 1 
-Config.Prompts = {
-    Collect = 'Collect Trap',
-    Check = 'Check Trap'
-}
 
 
 
